2bca9b4ff5233747c77a190f1ceee0eb2ba134ba
LauraKirby <laurakirby26@gmail.com>
2017-10-28 21:10:53 -0700

    continue with countdown timer and incorporate semantic html

--- spec/CountdownSpec.js
+++ spec/CountdownSpec.js
@@ -1,6 +1,7 @@
 describe('some test that needs a fixture', function(){
   beforeEach(function(){
-    var fixture = '<button class="primary-btn save-end-time" >Start countdown</button>' +
+    var fixture = '<p>Current time: <span class="current-time"></span></p>' +
+                  '<button class="primary-btn save-end-time" >Start countdown</button>' +
                   '<input type="text" name="end-hour">' +
                   '<input type="text" name="end-minute">';
 
@@ -11,8 +12,18 @@ describe('some test that needs a fixture', function(){
     document.body.remove(document.querySelector('save-end-time'));
   });
 
-  it('should have a button', function(){
-    var button = document.querySelector("button");
-    expect(button.innerText).toBe('Start countdown');
+  beforeEach(function() {
+    window.countdownTimer.init();
+  });
+
+  // it('should have a button', function(){
+  //   var button = document.querySelector("button");
+  //   expect(button.innerText).toBe('Start countdown');
+  // });
+
+  it('should display the current time', function(){
+    var currentTime = document.querySelector(".current-time");
+    var todaysDate = new Date;
+    expect(currentTime.innerText).toBe(todaysDate);
   });
 });
